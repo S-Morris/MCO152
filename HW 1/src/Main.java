46a5a8a3b9ba7ada312520c441f3f0b7ff17ead7
Shmuel <shmel1131@gmail.com>
2017-10-25 16:16:13 -0400

    .

--- HW 1/src/Main.java
+++ HW 1/src/Main.java
@@ -1,5 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("My first branch woohoo");
+        System.out.println("My first branch wooho");
     }
 }
